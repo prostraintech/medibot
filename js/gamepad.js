30d6c4a323aa400b392e4765002f8995659f64bb
0xABCDE <52780462+0xABCDE@users.noreply.github.com>
2020-08-12 11:23:29 +0800

    syntax err

--- js/gamepad.js
+++ js/gamepad.js
@@ -56,7 +56,7 @@ function getGamepadState() {
     
     if (buttonPressed(gamepad.buttons[0])) {
       pressed++;
-      if (pressed = 1)
+      if (pressed == 1)
       {
       socket.emit('button', "check temperature" )
       }
