d0745521cc846fc16b4175eb7692e00516cf50d0
aslammy <syedmuhdaslam98@gmail.com>
2020-09-01 16:35:57 +0800

    add sendstop

--- js/gamepad.js
+++ js/gamepad.js
@@ -32,44 +32,55 @@ function getGamepadState() {
         }
 		
 		var xAxis= gamepad.axes[0];
-		var yAxis= gamepad.axes[1];
+    var yAxis= gamepad.axes[1];
+    var sendstop = 0;
     
     
 		if (xAxis>-0.5 && xAxis<0.5 && yAxis<-0.1 && yAxis>-0.5)  //straight slow
 		{
 			//console.log("going right");
-			socket.emit('navi', 2);
+      socket.emit('navi', 2);
+      sendstop = 0;
 		}
 		else if (xAxis>-0.5 && xAxis<0.5 && yAxis<-0.5)       //straight fast
 		{
       //console.log("going left");
       socket.emit('navi', 4);
+      sendstop = 0;
 		}
 		
 		else if (xAxis>0.5 && yAxis<-0.25)                //turn right
 		{
       //socket.emit('navi', gamepad.axes[1]);
       socket.emit('navi', 6);
+      sendstop = 0;
     }
     
     else if (xAxis<-0.5 && yAxis<-0.25)                //turn left
 		{
       //socket.emit('navi', gamepad.axes[1]);
       socket.emit('navi', 8);
+      sendstop = 0;
     }
     
 		else if (xAxis>0.5 && yAxis>-0.25)            //rotate right
 		{
-			socket.emit('navi', 99);
+      socket.emit('navi', 99);
+      sendstop = 0;
     }
 
     else if (xAxis<-0.5 && yAxis>-0.25)            //rotate left
 		{
-			socket.emit('navi', 12);
+      socket.emit('navi', 12);
+      sendstop = 0;
     }
 
     else {
+      sendstop++;
+      if (sendstop<10)
+      {
       socket.emit('navi', 0);
+      }
     }
     
     if (buttonPressed(gamepad.buttons[0])) {
