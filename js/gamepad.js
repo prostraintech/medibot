4dee62313ed0e060fd8dad41662a1de9e0c6724c
aslammy <syedmuhdaslam98@gmail.com>
2020-09-04 16:35:29 +0800

    change intialization for cmd from local to global

--- js/gamepad.js
+++ js/gamepad.js
@@ -1,7 +1,6 @@
 const refreshRate = 50;
-const sameDataRate = 500;
 var pressed = 0;
-var cmd = 0;
+
 
 window.addEventListener("gamepadconnected", (event) => {
   console.log("A gamepad connected:");
@@ -15,88 +14,176 @@ window.addEventListener("gamepaddisconnected", (event) => {
   window.clearInterval(repGP);
 });
 
+//var gamepads = navigator.getGamepads();
+//console.log(gamepads);
+var count = 0;
+var cmd = 8;
+var val_joystick=0;
+
 function getGamepadState() {
 
-  // Returns up to 4 gamepads.
-  const gamepads = navigator.getGamepads();
+        // Returns up to 4 gamepads.
+        const gamepads = navigator.getGamepads();
 
-  // We take the first one, for simplicity
-  const gamepad = gamepads[0];
+        // We take the first one, for simplicity
+        const gamepad = gamepads[0];
 
-  // Escape if no gamepad was found
-  if (!gamepad) {
-    console.log('No gamepad found.');
-    return;
-  }
+        // Escape if no gamepad was found
+        if (!gamepad) {
+            console.log('No gamepad found.');
+            return;
+        }
 		
-  var xAxis= gamepad.axes[0];
-  var yAxis= gamepad.axes[1];
-  var pointTurnAxis= gamepad.axes[5];
-  var sendstop = 0;
-  var slow = 0;
-  var fast = 0;
-  var value = 0;
-
-  if (yAxis<-0.1 && yAxis>-0.5 && pointTurnAxis<0.5 && pointTurnAxis>-0.5 && !buttonPressed(gamepad.buttons[0]))  //straight slow
-  {
-    value = 1;
-  }
+		var xAxis= gamepad.axes[0];
+    var yAxis= gamepad.axes[1];
+    var pointTurnAxis= gamepad.axes[5];
+    var sendstop = 0;
+    var slow = 0;
+    var fast = 0;
+       
     
-  else if (yAxis<-0.5 && pointTurnAxis<0.5 && pointTurnAxis>-0.5 && !buttonPressed(gamepad.buttons[0]))       //straight fast
-  {
-    value = 2;
-  }
     
-  else if (pointTurnAxis>0.5 && !buttonPressed(gamepad.buttons[0]))       //rotate right
-  {
-    value = 3;
-  }
+		//if (xAxis>-0.5 && xAxis<0.5 && yAxis<-0.1 && yAxis>-0.5)  //straight slow
+		//{
+			//console.log("straight slow");
+      //socket.emit('navi', 1);
+     // sendstop = 0;
+    //}
+
+    if (yAxis<-0.1 && yAxis>-0.5 && pointTurnAxis<0.5 && pointTurnAxis>-0.5)  //straight slow
+		{
+      //console.log("straight slow");
+      //if (slow<1) {
+      //socket.emit('navi', 1);
+      val_joystick=1;
+     // }
+      //slow++;
+      //fast = 0;
+     // sendstop = 0;
+    }
+
+    else if (yAxis>0.1 && pointTurnAxis<0.5 && pointTurnAxis>-0.5)  //reverse slow
+		{
+      //console.log("straight slow");
+      //if (slow<1) {
+      //socket.emit('navi', 7);
+      val_joystick=7;
+     // }
+      //slow++;
+      //fast = 0;
+     // sendstop = 0;
+    }
     
-  else if (pointTurnAxis<-0.5 && !buttonPressed(gamepad.buttons[0]))       //rotate left
-  {
-    value = 4;
-  }
+		//else if (xAxis>-0.5 && xAxis<0.5 && yAxis<-0.5)       //straight fast
+		//{
+      //console.log("straight fast");
+      //socket.emit('navi', 2);
+      //sendstop = 0;
+    //}
     
-  else if (yAxis>0.1 && pointTurnAxis<0.5 && pointTurnAxis>-0.5)  //reverse slow
-  {
-    value = 7;
-  }
-
-  else {
-    value = 8;
-  }
+    else if (yAxis<-0.5 && pointTurnAxis<0.5 && pointTurnAxis>-0.5)       //straight fast
+		{
+      //console.log("straight fast");
+      //if (fast<1) {
+      //socket.emit('navi', 2);
+      val_joystick=2;
+      //}
+      //fast++;
+      //slow = 0;
+      //sendstop = 0;
+    }
     
-  if (cmd != value) {
-    cmd = value;
-    socket.emit('navi', cmd);
-  }
-
-  else if (cmd == value) {
-    sample = window.setInterval(DataSame, sameDataRate);
-  }
+    else if (pointTurnAxis>0.5)       //rotate right
+		{
+      //console.log("rotate right");
+      //socket.emit('navi', 3);
+      val_joystick=3;
+      //fast = 0;
+      //slow = 0;
+      //sendstop = 0;
+    }
     
-  if (buttonPressed(gamepad.buttons[0])) {
-    socket.emit('navi', 9);
-  }
-
-  else
-  {
-    pressed = 0;
-  }
-
-  }
-
-function DataSame() {
-  socket.emit('navi', cmd);
-}
-
-function buttonPressed(b) {
-  if (typeof(b) == "object") {
-    return b.pressed;
-  }
-  return b == 1.0;
-}
+    else if (pointTurnAxis<-0.5)       //rotate left
+		{
+      //console.log("rotate left");
+      //socket.emit('navi', 4);
+      val_joystick=4;
+      //fast = 0;
+      //slow = 0;
+      //sendstop = 0;
+		}
+		
+		//else if (xAxis>0.5 && yAxis<-0.25)                //turn right
+		//{
+      //socket.emit('navi', gamepad.axes[1]);
+      //socket.emit('navi', 3);
+      //sendstop = 0;
+    //}
     
-socket.on('navi', function(msg){
-console.log(msg);
-});
+    //else if (xAxis<-0.5 && yAxis<-0.25)                //turn left
+		//{
+      //socket.emit('navi', gamepad.axes[1]);
+      //socket.emit('navi', 4);
+      //sendstop = 0;
+    //}
+    
+		//else if (xAxis>0.5 && yAxis>-0.25)            //rotate right
+		//{
+      //socket.emit('navi', 5);
+      //sendstop = 0;
+    //}
+
+    //else if (xAxis<-0.5 && yAxis>-0.25)            //rotate left
+		//{
+      //socket.emit('navi', 6);
+      //sendstop = 0;
+    //}
+
+    else {
+      //sendstop++;
+      //if (sendstop==1)
+      //{
+        //socket.emit('navi', 8);
+        val_joystick = 8;
+      //}
+    }
+    
+    //if (buttonPressed(gamepad.buttons[0])) {
+      //if (pressed<1)
+      //{
+      //socket.emit('navi', 9)
+      //}
+      //pressed++;
+      
+      if (cmd != val_joystick)
+      {
+        cmd = val_joystick;
+        socket.emit('navi',cmd);
+        count = 0;
+      }
+      else
+        count++;
+      if (count >=1000 ){
+         socket.emit('navi',cmd);
+         count = 0;
+    }
+
+    // else
+    // {
+    //   pressed = 0;
+    // }
+
+    // }
+  }
+    function buttonPressed(b) {
+      if (typeof(b) == "object") {
+        return b.pressed;
+      }
+      return b == 1.0;
+    }
+	
+
+	
+		 socket.on('navi', function(msg){
+		 console.log(msg);
+		 });
