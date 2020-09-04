cc535181f0bb36ee0be4351826e77eb9f0f18553
0xABCDE <52780462+0xABCDE@users.noreply.github.com>
2020-09-04 15:07:38 +0800

    update

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ socket.on('resp', (status) => {
   //console.log(status);
   //var res = 1100-(Math.trunc((Math.sqrt(Math.pow(status*1000,2))))).toString();
   //arduinoSerialPort.write(res+'\n');
-  //console.log(status);
+  console.log("berrymed data rcecived");
     
 });
 
