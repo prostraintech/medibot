9fa27024c279c9029b7d5bc8dadb3baaa749510a
afifhusman <69129166+afifhusman@users.noreply.github.com>
2020-09-09 02:42:44 +0800

    test navi

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
 
   socket.on('navi', (status) => {
     socket.emit('navi', status);
-    //console.log("ni'm here at navi");
+    console.log("i'm here at navi");
     if (cmd != status) {
       cmd = status;
       var res = cmd.toString();
