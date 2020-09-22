a5d797de2ae0f43ceae1668ec24636fb9c6362ab
afifhusman <69129166+afifhusman@users.noreply.github.com>
2020-09-22 09:34:09 +0800

    add git pull to shutdown

--- app.js
+++ app.js
@@ -118,14 +118,14 @@ io.on('connection', (socket) => {
 
   socket.on('shutdown', (shut) => {
     const { exec } = require('child_process');
-exec('ls | grep js', (err, stdout, stderr) => {
+exec('git pull', (err, stdout, stderr) => {
   if (err) {
     //some err occurred
     console.error(err)
   } else {
    // the *entire* stdout and stderr (buffered)
-   console.log(`stdout: ${stdout}`);
-   console.log(`stderr: ${stderr}`);
+   console.log(`${stdout}`);
+   //console.log(`stderr: ${stderr}`);
   }
 });
 
