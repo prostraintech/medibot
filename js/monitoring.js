a7c57c54b04e87ed969d78b427c969ac61d5a0da
Aiman5697 <70120443+Aiman5697@users.noreply.github.com>
2020-09-24 15:59:36 +0800

    add battery icon

--- js/monitoring.js
+++ js/monitoring.js
@@ -11,14 +11,28 @@ socket.on('vbat', (vbat) => {
     //console.log("battery -> " + vbat +"V");
     document.getElementById("top_battery").innerHTML = monitoring[0] + "V";
     //console.log("im here in connect");
-    if (volt == 20)
+    
+    else if (volt == 23)  {
+      document.getElementById("top_battery").className = "fas fa-battery-three-quarters";
+    }
+
+    else if (volt == 22)
+    {
+    document.getElementById("top_battery").className = "fas fa-battery-half";
+    }
+
+    else if (volt == 21)
+    {
+    document.getElementById("top_battery").className = "fas fa-battery-quarter";
+    }
+    
+    else if (volt == 20)
     {
     document.getElementById("top_battery").className = "fas fa-battery-empty";
     }
 
     var temp = parseInt(monitoring[1]);
     document.getElementById("battery_temp").innerHTML = monitoring[1] + "\u00B0" + "C";
-    console.log("\u00B0");
 
 
   });
