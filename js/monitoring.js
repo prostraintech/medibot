7d6a5ba25bd738cf8eba65c7f90c8851bcaeb208
Aiman5697 <70120443+Aiman5697@users.noreply.github.com>
2020-09-24 15:13:57 +0800

    try battery empty

--- js/monitoring.js
+++ js/monitoring.js
@@ -6,14 +6,16 @@ socket.on('vbat', (vbat) => {
     var monitoring = vbat.split(" ");
 
     var volt = parseInt(monitoring[0]);
-    console.log(volt);
+    // console.log(volt);
     //socket.emit('data',data);
     //console.log("battery -> " + vbat +"V");
     document.getElementById("top_battery").innerHTML = monitoring[0] + "V";
     //console.log("im here in connect");
-    if (debugc == 1)
+    if (volt == 24)
     {
     document.getElementById("top_battery").className = "fas fa-battery-empty";
     }
+
+
 
   });
