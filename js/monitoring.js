a674495fd45d41ec773e1dada4a14b4eb543806a
afifhusman <69129166+afifhusman@users.noreply.github.com>
2020-09-24 10:45:58 +0800

    debug

--- js/monitoring.js
+++ js/monitoring.js
@@ -1,7 +1,6 @@
-
 socket.on('vbat', (vbat) => {
     //socket.emit('data',data);
-    console.log("battery -> " + vbat +"V");
+    //console.log("battery -> " + vbat +"V");
     document.getElementById('statusbar').innerHTML = vbat +"V";
     //console.log("im here in connect");
   });
