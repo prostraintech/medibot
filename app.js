9cd820b42c2a82ef95851bb8458bc62723823335
afifhusman <69129166+afifhusman@users.noreply.github.com>
2020-09-24 11:05:19 +0800

    add fontawesome - aslam punya pasal

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ arduinoSerialPort.on('close', function () {
 
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/fav'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/webfonts'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
