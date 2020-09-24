cfc8efa5a8eae4ee855435870e3876d0c12d7523
afifhusman <69129166+afifhusman@users.noreply.github.com>
2020-09-24 11:24:21 +0800

    add battery low condition

--- js/monitoring.js
+++ js/monitoring.js
@@ -1,6 +1,14 @@
+var debugc = 0;
+
 socket.on('vbat', (vbat) => {
+
     //socket.emit('data',data);
     //console.log("battery -> " + vbat +"V");
     document.getElementById('statusbar').innerHTML = vbat.toString() + "V";
     //console.log("im here in connect");
+    if (debugc == 1)
+    {
+    document.getElementById("top_battery").className = "fas fa-battery-empty";
+    }
+
   });
