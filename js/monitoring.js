aefe88734a419e8b5abf357ba00ef4261255edb7
Aiman5697 <70120443+Aiman5697@users.noreply.github.com>
2020-09-24 14:27:14 +0800

    vbat as array

--- js/monitoring.js
+++ js/monitoring.js
@@ -2,9 +2,10 @@ var debugc = 0;
 
 socket.on('vbat', (vbat) => {
 
+    var res = vbat.split(" ");
     //socket.emit('data',data);
     //console.log("battery -> " + vbat +"V");
-    document.getElementById("top_battery").innerHTML = vbat.toString() + "V";
+    document.getElementById("top_battery").innerHTML = vbat[0].toString() + "V";
     //console.log("im here in connect");
     if (debugc == 1)
     {
