7e299e114fc6c2c5aa5e3a9ef7b4fc47b5068599
Aiman5697 <70120443+Aiman5697@users.noreply.github.com>
2020-09-24 14:32:35 +0800

    change res to table

--- js/monitoring.js
+++ js/monitoring.js
@@ -2,10 +2,10 @@ var debugc = 0;
 
 socket.on('vbat', (vbat) => {
 
-    var res = vbat.split(" ");
+    var table = vbat.split(" ");
     //socket.emit('data',data);
     //console.log("battery -> " + vbat +"V");
-    document.getElementById("top_battery").innerHTML = vbat[0].toString() + "V";
+    document.getElementById("top_battery").innerHTML = table[0].toString() + "V";
     //console.log("im here in connect");
     if (debugc == 1)
     {
