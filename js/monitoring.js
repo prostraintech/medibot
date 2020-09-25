8f12093de6da57a50b654c2a22682d1a02a4c3c2
Aiman5697 <70120443+Aiman5697@users.noreply.github.com>
2020-09-25 10:01:34 +0800

    make condition for 24V

--- js/monitoring.js
+++ js/monitoring.js
@@ -10,9 +10,13 @@ socket.on('vbat', (vbat) => {
     //socket.emit('data',data);
     //console.log("battery -> " + vbat +"V");
     document.getElementById("top_battery").innerHTML = " " + monitoring[0] + "V";
+    if (volt == 24) {
+      document.getElementById("top_battery").className = "fas fa-battery-full";
+    }
+    
     //console.log("im here in connect");
     
-    if (volt == 23)  {
+    else if (volt == 23)  {
       document.getElementById("top_battery").className = "fas fa-battery-three-quarters";
     }
 
