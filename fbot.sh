c292784adb961996b98e7ac41c864a6383d61dff
mic-710aix <mic-710aix@ubuntu.ubuntu>
2021-10-28 14:37:29 +0800

    updated fbot

--- fbot.sh
+++ fbot.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+git pull
 ./arduino-cli compile --fqbn per1234:sam:arduino_due_x_dbg flashbot && ./arduino-cli upload --port /dev/ttyACM0 --fqbn per1234:sam:arduino_due_x_dbg flashbot
+
+if [ "$1" == "debug" ]
+then
+ minicom -D /dev/ttyACM0 -b 9600
+fi
