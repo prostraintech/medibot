eec3594d03571ebf6d2a1db7da5e5c66621fbc89
afifhusman <69129166+afifhusman@users.noreply.github.com>
2021-10-28 10:40:03 +0800

    Update flashbot.ino

--- flashbot/flashbot.ino
+++ flashbot/flashbot.ino
@@ -47,7 +47,7 @@ void loop() {
 
   
     digitalWrite(LED_R_LH,0);
-    digitalWrite(LED_G_LH,255);
+    digitalWrite(LED_G_LH,254);
     digitalWrite(LED_B_LH,0); //red
     digitalWrite(LED_R_RH,254);
     digitalWrite(LED_G_RH,254);
