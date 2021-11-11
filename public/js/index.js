aca7b35c41442518ad9a31b513b741cca4c36bcf
Your Name <you@example.com>
2021-11-11 13:05:27 +0800

    cam

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,14 @@ var socket = io();
 var el;
 var constraints = {};
 var elemVid = document.getElementById("remotevideo");
-var dID;
+var dIDnavi;
 
 var videol = document.querySelector("#videoElement");
 
   const constraints_navi = {
         'audio': {'echoCancellation': true},
         'video': {
-            'deviceId': 'b18c34404595c0cbdb4dd1bcf146b46f709d6357a6a2428910d3b4d17c7f4d5f'
+            'deviceId': dID
             }
         }
 
@@ -20,12 +20,20 @@ var videol = document.querySelector("#videoElement");
             }
         }
 
-  function getConnectedDevices(label, callback) {
+  function getAllConnectedDevices(label, callback) {
     navigator.mediaDevices.enumerateDevices()
         .then(devices => {
-            const filtered = devices.filter(device => device.label === label);
+            const filtered = devices.filter(device => device.kind === label);
             callback(filtered);
         });
+}
+
+function getConnectedDevices(label, callback) {
+  navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+          const filtered = devices.filter(device => device.label === label);
+          callback(filtered);
+      });
 }
 
 try{
@@ -37,6 +45,16 @@ getConnectedDevices('Logitech B525 HD Webcam (046d:0836)', cameras => {
 catch{
   console.log('no such input here');
 }
+
+try{
+  getAllConnectedDevices('videoinput', cameras => {
+    console.log('Cameras found', cameras);
+    //console.log(cameras[0].deviceId);
+  });
+  }
+  catch{
+    console.log('no such input here');
+  }
 
 
 //frontcam - 6e5b4c36dc543308887c116abf5fac376a78987375fea5cce8fc254b6a145723
