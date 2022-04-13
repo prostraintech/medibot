95b388913e56406693c248af5074e7a5bfc8f62c
afif <afifhusman@live.iium.edu.my>
2022-04-13 22:07:45 +0800

    npm update

--- fbot.sh
+++ fbot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git pull
-./arduino-cli compile --fqbn per1234:sam:arduino_due_x_dbg flashbot && ./arduino-cli upload --port /dev/ttyACM0 --fqbn per1234:sam:arduino_due_x_dbg flashbot
+./arduino-cli compile --fqbn per1234:sam:arduino_due_x_dbg flashbot && ./arduino-cli upload --port /dev/ttyACM1 --fqbn per1234:sam:arduino_due_x_dbg flashbot
 
 if [ "$1" == "debug" ]
 then
- minicom -D /dev/ttyACM0 -b 9600
+ minicom -D /dev/ttyACM1 -b 9600
 fi
--- ucam.sh
+++ ucam.sh
@@ -1,11 +1,11 @@
 export DBUS_FATAL_WARNINGS=0
 sudo rmmod v4l2loopback
-sudo modprobe v4l2loopback devices=2 video_nr=11,22 card_label="front_cam","back_cam" exclusive_caps=1,1
-sudo modprobe -r snd-aloop
-sudo modprobe snd-aloop
+sudo modprobe v4l2loopback devices=1 video_nr=11 card_label="front_cam" exclusive_caps=1
+#sudo modprobe -r snd-aloop
+#sudo modprobe snd-aloop
 
-pactl load-module module-pipe-source source_name=frontmic file=/tmp/frontmic format=s16le rate=16000 channels=1
+#pactl load-module module-pipe-source source_name=frontmic file=/tmp/frontmic format=s16le rate=16000 channels=1
 
 ffmpeg -threads 1 -i rtsp://admin:MedibotV4@192.168.1.50/Streaming/Channels/101 \
--map 0:0 -f v4l2 -pix_fmt yuv420p -r 15 -b:v 1024 -tune zerolatency /dev/video11 \
--map 0:1 -f s16le -ar 16000 -ac 1 - > /tmp/frontmic
+-map 0:0 -f v4l2 -pix_fmt yuv420p -r 15 -b:v 1024 -tune zerolatency /dev/video11
+#-map 0:1 -f s16le -ar 16000 -ac 1 - > /tmp/frontmic
